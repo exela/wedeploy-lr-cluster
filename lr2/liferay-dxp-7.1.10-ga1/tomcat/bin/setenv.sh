49ce152d8ec6b81152b4e8942d9909c8cdc8f8ea
Exela <exela@exela.local>
2018-07-24 11:40:46 -0700

    Include new cluster properties

--- lr2/liferay-dxp-7.1.10-ga1/tomcat/bin/setenv.sh
+++ lr2/liferay-dxp-7.1.10-ga1/tomcat/bin/setenv.sh
@@ -1,1 +1,1 @@
-CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx4g -XX:MaxMetaspaceSize=2g -Djgroups.bind_addr=lr2 -Djgroups.tcpping.initial_hosts=lr1[8080],lr2[8080],lr3[8080]"
+CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx4g -XX:MaxMetaspaceSize=4g"
